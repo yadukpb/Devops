3ab3d6c4878c6dcc196b6a58a2710325d9c0ca71
shiraz <shirazyousuf1234@gmail.com>
2024-09-06 17:10:12 +0530

    change

--- shiraz.py
+++ shiraz.py
@@ -1,1 +1,12 @@
-print("Devops")
+def full_pyramid(n):
+    for i in range(1, n + 1):
+        # Print leading spaces
+        for j in range(n - i):
+            print(" ", end="")
+        
+        # Print asterisks for the current row
+        for k in range(1, 2*i):
+            print("*", end="")
+        print()
+   
+full_pyramid(5)
