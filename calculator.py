134b8f734f60f482c241991e3111fe0f99daa7fc
Karan <karanraaj2003@gmail.com>
2024-09-13 17:12:17 +0530

    Subtraction function added

--- calculator.py
+++ calculator.py
@@ -2,13 +2,19 @@
 def add(a, b):
     return a + b
 
+#author -@KaranRaaj
+def sub(a,b):
+    return a-b
+
 def main():
     print(" Calculator ")
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
         result = add(num1, num2)
-        print(f"The result of {num1} + {num2} is: {result}")
+        result1 = sub(num1,num2)
+        print(f"The result of adding {num1} + {num2} is: {result}")
+        print(f"The difference between {num1} + {num2} is: {result1}")
     except ValueError:
         print("Invalid input. Please enter numeric values.")
 
