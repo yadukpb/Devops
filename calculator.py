343dc4e88733b5a72c20a53f39f5ef585bf234ae
yadukrishnapb~ <yadukrishnapb1@gmail.com>
2024-09-13 16:57:41 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#author -@Yadukrishna
+def add(a, b):
+    return a + b
+
+def main():
+    print(" Calculator ")
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = add(num1, num2)
+        print(f"The result of {num1} + {num2} is: {result}")
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
+
+if __name__ == "__main__":
+    main()
