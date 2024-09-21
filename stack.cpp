2387fd0f63a3c9094e5e99b3dd623df7052aaeee
yadukrishnapb~ <yadukrishnapb1@gmail.com>
2024-09-21 23:22:00 +0530

    stack push

--- stack.cpp
+++ stack.cpp
@@ -1,1 +1,27 @@
 // Implementation of Stack code in cpp
+
+#include <iostream>
+#include <vector>
+
+class Stack {
+private:
+    std::vector<int> elements;
+
+public:
+// @yadukpb - Added a push function to push an element to stack
+    void push(int value) {
+        elements.push_back(value);
+    }
+};
+
+int main() {
+    Stack stack;
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    std::cout << "Elements pushed to the stack: 10, 20, 30" << std::endl;
+
+    return 0;
+}
