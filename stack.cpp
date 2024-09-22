159edf9fb6569531349187d4d6750f4a3e4db9dc
shiraz <shirazyousuf1234@gmail.com>
2024-09-22 10:44:48 +0530

    added a function to search for an element in a stack

--- stack.cpp
+++ stack.cpp
@@ -11,43 +11,42 @@ private:
     std::vector<int> elements;
 
 public:
-// @yadukpb - Added a push function to push an element to stack
+    // @yadukpb - Added a push function to push an element to stack
     void push(int value) {
         elements.push_back(value);
     }
 
-// @pratik - Added a function that returns the top of a stack
+    // @pratik - Added a function that returns the top of a stack
     int peek() {
-    if (elements.empty()) {
-        std::cout << "Error: Stack is empty" << std::endl;
-        return -1;
-    }
-    return elements.back();
+        if (elements.empty()) {
+            std::cout << "Error: Stack is empty" << std::endl;
+            return -1;
+        }
+        return elements.back();
     }
 
     //delete from stack : Added by adnan
     int pop() {
-    int top = elements.back();
-    elements.pop_back();
-    return top;
+        int top = elements.back();
+        elements.pop_back();
+        return top;
     }
     //@Sithartha - Checking whether the stack is empty or not.
-    bool isEmpty() {
-        
-    return elements.empty();
+    bool isEmpty() {  
+        return elements.empty();
     }
 
 
     //@karan - reversing the stack elements
     void reverseStack(Stack &s) {
-    Stack auxStack;
-    while (!s.isEmpty()) {
-        auxStack.push(s.pop());
+        Stack auxStack;
+        while (!s.isEmpty()) {
+            auxStack.push(s.pop());
+        }
+        while (!auxStack.isEmpty()) {
+            s.push(auxStack.pop());
+        }
     }
-    while (!auxStack.isEmpty()) {
-        s.push(auxStack.pop());
-    }
-}
 
     // @maurya - Added function print()
     void print() {
@@ -60,16 +59,27 @@ public:
 
     //@lohith - Added a function that returns the min element of the stack
     int getMin() {
-    if (elements.empty()) {
-        std::cout << "Error: Stack is empty" << std::endl;
-        return -1;
-    }
-    return *std::min_element(elements.begin(), elements.end());
+        if (elements.empty()) {
+            std::cout << "Error: Stack is empty" << std::endl;
+            return -1;
+        }
+        return *std::min_element(elements.begin(), elements.end());
     }
 
     //@praneeth - Added a function that returns the size of the stack
     int size() {
-    return elements.size();
+        return elements.size();
+    }
+
+    //@shiraz - Added a function that searches for an element in a stack
+    bool searchInStack(int target) {
+        while (!elements.empty()) {
+            if (elements.top() == target) {
+                return true; // Element found
+            }
+            s.pop(); // Remove the top element
+        }
+        return false; // Element not found
     }
 
 };
@@ -84,7 +94,7 @@ int main() {
     stack.pop();
     
     stack.reverseStack(stack);
-
+    stack.searchInStack(20);
     stack.print();
     
     int x = stack.getMin();
